ab22d0fff4eae46ee4e6d41f6b52a0877ba06c13
Peny <43066291+pee331@users.noreply.github.com>
2025-10-19 17:55:23 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/apigateway/main.go
@@ -0,0 +1,23 @@
+// cmd/apigateway/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "apigateway/internal/apigateway"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := apigateway.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
